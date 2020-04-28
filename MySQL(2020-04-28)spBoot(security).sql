37b346b204b8132052146466ce9e2ebaa09950bf
freeswoo <freeswoo@naver.com>
2020-04-28 16:38:44 +0900

    2020-04-28

---
+++ MySQL(2020-04-28)spBoot(security).sql
@@ -0,0 +1,14 @@
+-- 새로운 데이터 베이스 생성
+create database spBoot;
+use spBoot;
+
+DESC tbl_users;
+drop table tbl_users;
+drop table hibernate_sequence;
+
+select * from tbl_users;
+SELECT * FROM tbl_roles;
+desc tbl_users;
+desc tbl_roles;
+
+
